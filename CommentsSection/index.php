33e55fa29be03f4f50bf5d8df21c64c4a6144373
Daniel DeMuro <danieljd489@gmail.com>
2016-03-23 14:47:51 -0400

    Third Commit

---
+++ CommentsSection/index.php
@@ -0,0 +1,51 @@
+<?php
+require('connect.php');
+$name=$_POST['name'];
+$comment=$_POST['comment'];
+$submit=$_POST['submit'];
+
+if($submit)
+{
+  if($name&&$comment)
+  {
+   $insert=mysql_query("INSERT INTO comment (name,comment) VALUES ('$name','$comment')");
+   header("Location: repeat.php");
+  }
+  else
+  {
+    echo "Please fill all fields";
+  }
+}
+
+
+?>
+<html>
+<head>
+<title>Comment Section</title>
+</head>
+
+<body>
+<form action="index.php" method="POST">
+<table>
+<tr><td>Name:</td><td><input type="text" name="name"/></td></tr>
+<tr><td colspan="2">Comment: </td></tr>
+<tr><td colspan="2"><textarea name="comment"></textarea></td></tr>
+<tr><td colspan="2"><input type="submit" name="submit" value="Comment" /></td></tr>
+</table>
+</form>
+<?php
+$getquery=mysql_query("SELECT * FROM comment ORDER BY id DESC")
+while($rows=mysql_fetch_assoc($getquery))
+{
+  $id=$rows['id'];
+  $name=$rows['name'];
+  $comment=$rows['comment'];
+  $dellink="<a href=\"delete.php?Id=" . $id . "\"> Delete </a>";
+  echo $name . '<br />' . '<br />' . $comment . '<br />' . $dellink . '<br />' . '<hr width="250px" />';
+}
+
+
+
+?>
+</body>
+</html>
---
+++ SearchBar/searchbar.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>Search Podcasts</title>
+  </head>
+  <p><body>
+    <h3>Search Podcast Details</h3>
+    <p>Search by keyword or name</p>
+    <form method="post" action="search.php?go" id="searchform">
+      <input type="text" name="name">
+      <input type="submit" name="submit" value="Search">
+    </form>
+  </body>
+</html>
+</p>
+
